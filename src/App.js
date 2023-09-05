d38c8eba1d3bb9d85907ca53033fc7c0969b99da
Disskretnost <122427143+Disskretnost@users.noreply.github.com>
2023-09-05 12:57:31 +0300

    entering a note

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header.jsx';
+import CreateToDo from './components/CreateToDo/CreateToDo';
+import { useEffect,useState } from 'react';
+
 
 function App() {
+
+  const [toDoList, setToDoList] = useState([])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+          <Header />
+          <CreateToDo />
+      </div>
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <>
+      <h1 className="title"> My TodoList</h1>
+      <p className="subtitle">A simple list app using json server</p>
+    </>
+  )
+}
+
+export default Header
